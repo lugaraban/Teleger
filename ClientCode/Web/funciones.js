5261f9c44b0b69b6af973f3bcb3dad4ee551b4da
lugaraban <lugaraban@gmail.com>
2016-04-14 01:56:38 +0200

    Some modifications in the web code

--- ClientCode/Web/funciones.js
+++ ClientCode/Web/funciones.js
@@ -4,12 +4,12 @@ function registro(){
 	var contrasena=document.getElementById("password");
 	var imagen=document.getElementById("imagen");
 
-	onRegister(alias, contrasena, name, imagen);
+	app.register(alias, contrasena, name, imagen);
 }
 
 function logIn(){
 	var alias=document.getElementById("alias");
 	var contrasena=document.getElementById("contrasena");
 
-	onLogIn(alias, contrasena);
+	app.LogIn(alias, contrasena);
 }
