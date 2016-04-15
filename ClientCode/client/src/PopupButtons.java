e29481415b71868b4be015b3dc2f233bd54a0b34
lugaraban <lugaraban@gmail.com>
2016-04-16 00:57:06 +0200

    We are almost there!!

--- ClientCode/client/src/PopupButtons.java
+++ ClientCode/client/src/PopupButtons.java
@@ -13,6 +13,7 @@ import java.awt.Color;
 import javax.swing.JLabel;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import javax.swing.SwingConstants;
 
 public class PopupButtons extends JDialog {
 
@@ -22,11 +23,10 @@ public class PopupButtons extends JDialog {
 	/**
 	 * Create the dialog.
 	 */
-	public PopupButtons(String mensaje, JFrame panel, Boolean response) {
+	public PopupButtons(String mensaje, JFrame panel, String userId, String userPassword, String friend, ServerInterface server) {
 		setTitle("NOTIFICACIÓN");
 		setResizable(false);
 		setModal(false);
-		this.res=response;
 		
 		//this.setAlwaysOnTop(true);
 		this.setLocationRelativeTo(panel);
@@ -36,42 +36,40 @@ public class PopupButtons extends JDialog {
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
 		getContentPane().add(contentPanel, BorderLayout.CENTER);
 		contentPanel.setLayout(null);
+		JButton okButton = new JButton("Accept");
+		okButton.setBounds(0, 62, 178, 23);
+		contentPanel.add(okButton);
+		okButton.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				res=true;
+				server.notifyAnswerRequest(userId, userPassword, friend, res);
+				dispose();
+			}
+		});
+		okButton.setBackground(new Color(60, 179, 113));
+		okButton.setActionCommand("OK");
+		getRootPane().setDefaultButton(okButton);
 		{
-			JLabel lblRequest = new JLabel(mensaje);
-			lblRequest.setBounds(322, 0, -320, 52);
-			contentPanel.add(lblRequest);
+			JButton cancelButton = new JButton("Reject");
+			cancelButton.setBounds(178, 62, 149, 23);
+			contentPanel.add(cancelButton);
+			cancelButton.addMouseListener(new MouseAdapter() {
+				@Override
+				public void mouseClicked(MouseEvent e) {
+					res=false;
+					server.notifyAnswerRequest(userId, userPassword, friend, res);
+					dispose();
+				}
+			});
+			cancelButton.setBackground(new Color(250, 128, 114));
+			cancelButton.setActionCommand("Cancel");
 		}
 		{
-			JPanel buttonPane = new JPanel();
-			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
-			getContentPane().add(buttonPane, BorderLayout.SOUTH);
-			{
-				JButton okButton = new JButton("Accept");
-				okButton.addMouseListener(new MouseAdapter() {
-					@Override
-					public void mouseClicked(MouseEvent arg0) {
-						res=true;
-						dispose();
-					}
-				});
-				okButton.setBackground(new Color(60, 179, 113));
-				okButton.setActionCommand("OK");
-				buttonPane.add(okButton);
-				getRootPane().setDefaultButton(okButton);
-			}
-			{
-				JButton cancelButton = new JButton("Reject");
-				cancelButton.addMouseListener(new MouseAdapter() {
-					@Override
-					public void mouseClicked(MouseEvent e) {
-						res=false;
-						dispose();
-					}
-				});
-				cancelButton.setBackground(new Color(250, 128, 114));
-				cancelButton.setActionCommand("Cancel");
-				buttonPane.add(cancelButton);
-			}
+			JLabel lblRequest = new JLabel(mensaje);
+			lblRequest.setHorizontalAlignment(SwingConstants.CENTER);
+			lblRequest.setBounds(0, 0, 327, 59);
+			contentPanel.add(lblRequest);
 		}
 	}
 
